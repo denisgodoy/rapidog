dda8bc9bc8fb6b7ce4b5de5b11d4714007a7c1e7
denisgodoy <denisgodoy1@gmail.com>
2019-11-28 20:33:28 -0300

    placeholder

--- rapidog/views.py
+++ rapidog/views.py
@@ -6,9 +6,9 @@ def index(request):
     return render(request, 'index.html')
 
 def produtos(request):
-    secao_produtos = Produto.objects.filter(disponivel=True).order_by('-id')
+    # secao_produtos = Produto.objects.filter(disponivel=True).order_by('-id')
 
-    context = {
-        'produtos': secao_produtos
-    }
-    return render(request, 'produtos.html', context)
+    # context = {
+    #     'produtos': secao_produtos
+    # }
+    return render(request, 'produtos.html')
