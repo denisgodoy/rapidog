eb3c8e635f72dad859410b0ddda5c1e00fa26079
denisgodoy <denisgodoy1@gmail.com>
2019-11-28 20:30:27 -0300

    js efeito mouseover produtos

--- rapidog/static/scripts.js
+++ rapidog/static/scripts.js
@@ -1,14 +1,23 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    // jQuery('.navFixa').css("background-color", "rgba(255, 255, 255, 0.2)");
     jQuery('.navFixa').css("background-color", "");
     jQuery(window).scroll(function() {
         if (jQuery(window).scrollTop() > 10) {
-            jQuery('.navFixa').css("background-color", "white");
-            jQuery('.navFixa a').css("color", "#0E1122");            
+            jQuery('.navFixa').css("background-color", "#0E1122");
+            jQuery('.navFixa a').css("color", "white");            
         } else {
-            // jQuery('.navFixa').css("background-color", "rgba(255, 255, 255, 0.2)");
             jQuery('.navFixa').css("background-color", "");
             jQuery('.navFixa a').css("color", "white"); 
         }
     });
 });
+
+// function mouseOver() {
+//     document.getElementById("produto").style.backgroundColor = "rgb(245, 245, 245)";
+//   }
+  
+//   function mouseOut() {
+//     document.getElementById("produto").style.backgroundColor = "white";
+//   }
+
+// document.getElementById("produto").onmouseover = function() {mouseOver()};
+// document.getElementById("produto").onmouseout = function() {mouseOut()};
