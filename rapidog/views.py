a2148358f0bc93ee11b0ab9d3ac7628898cb22d0
denisgodoy <denisgodoy1@gmail.com>
2019-11-27 23:28:19 -0300

    adicionar return def produto

--- rapidog/views.py
+++ rapidog/views.py
@@ -6,7 +6,9 @@ def index(request):
     return render(request, 'index.html')
 
 def produtos(request):
-    secao_produtos = Produto.objects.filter(disponivel=True)
+    secao_produtos = Produto.objects.filter(disponivel=True).order_by('-id')
+
     context = {
         'produtos': secao_produtos
     }
+    return render(request, 'produtos.html', context)
