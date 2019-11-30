256b348eaff9ba580a67ddec12051dd0d177da1d
denisgodoy <denisgodoy1@gmail.com>
2019-11-30 03:03:51 -0300

    configurar views para categorias de produtos no html

--- rapidog/views.py
+++ rapidog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rapidog.models import *
 
 # Create your views here.
 
@@ -6,9 +7,14 @@ def index(request):
     return render(request, 'index.html')
 
 def produtos(request):
-    # secao_produtos = Produto.objects.filter(disponivel=True).order_by('-id')
+    alimentacao = Produto.objects.filter(categoria='alimentacao').order_by('-id')[:5]
+    higiene = Produto.objects.filter(categoria='higiene').order_by('-id')[:5]
+    brinquedos = Produto.objects.filter(categoria='brinquedos').order_by('-id')[:5]
 
-    # context = {
-    #     'produtos': secao_produtos
-    # }
-    return render(request, 'produtos.html')
+    context = {
+        'alimentacao': alimentacao,
+        'higiene': higiene,
+        'brinquedos': brinquedos
+    }
+
+    return render(request, 'produtos.html', context)
