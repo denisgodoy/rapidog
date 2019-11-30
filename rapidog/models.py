4144f1d7f483f44b42fcd121dd12f8efbb0ffa12
denisgodoy <denisgodoy1@gmail.com>
2019-11-30 02:57:24 -0300

    atualizar e migrar models

--- rapidog/models.py
+++ rapidog/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Produto(models.Model):
     tipos_produto = [
-    ('racao', 'Ração'),
-    ('acessorio', 'Acessórios'),
-    ('higi', 'Higiene'),
-    ('briquedos', 'Brinquedos'),
+    ('alimentacao', 'Alimentação'),
+    ('acessorios', 'Acessórios'),
+    ('higiene', 'Higiene'),
+    ('brinquedos', 'Brinquedos'),
     ]
 
     nome_produto = models.CharField(max_length=80)
@@ -16,7 +16,9 @@ class Produto(models.Model):
     preco = models.DecimalField(decimal_places=2, max_digits=20, default=0)
     disponivel = models.BooleanField(default=True)
     quantidade = models.IntegerField(default=1)
-    categoria = models.CharField(max_length=2, choices=tipos_produto)
+    categoria = models.CharField(max_length=15, choices=tipos_produto)
+    descricao = models.CharField(max_length=400)
+    imagem = models.ImageField(upload_to='media/', null=True, blank=True)
 
     def __str__(self):
-        return self.nome
+        return self.nome_produto
