b432982be790f4802a29f05275a5778f42ef3933
denisgodoy <denisgodoy1@gmail.com>
2019-11-30 04:26:45 -0300

    re-migrar campo upload_to corrigido

--- rapidog/models.py
+++ rapidog/models.py
@@ -18,7 +18,7 @@ class Produto(models.Model):
     quantidade = models.IntegerField(default=1)
     categoria = models.CharField(max_length=15, choices=tipos_produto)
     descricao = models.CharField(max_length=400)
-    imagem = models.ImageField(upload_to='media/', null=True, blank=True)
+    imagem = models.ImageField(upload_to='', null=True, blank=True)
 
     def __str__(self):
         return self.nome_produto
