3b3a828b79a0324c599d850f1ca37e420dcd7464
denisgodoy <denisgodoy1@gmail.com>
2019-11-30 18:38:57 -0300

    adicionar serviços na Model Loja e migrar

--- rapidog/models.py
+++ rapidog/models.py
@@ -48,6 +48,12 @@ class Loja(models.Model):
     estado = models.CharField(max_length=2, choices=estados)
     verificado = models.BooleanField(default=True)
     logotipo = models.ImageField(upload_to='', null=True, blank=True)
+    banho = models.BooleanField(default=True)
+    preco_banho = models.DecimalField(decimal_places=2, max_digits=20, default=0, blank=True, null=True)
+    tosa = models.BooleanField(default=True)
+    preco_tosa = models.DecimalField(decimal_places=2, max_digits=20, default=0, blank=True, null=True)
+    combo = models.BooleanField(default=True)
+    preco_combo = models.DecimalField(decimal_places=2, max_digits=20, default=0, blank=True, null=True)
 
     def __str__(self):
         return self.nome_loja
