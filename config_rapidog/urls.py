a9f384745a383e3b2bfe923dba4316d5b4037270
denisgodoy <denisgodoy1@gmail.com>
2019-12-03 20:06:34 -0300

    criação de página e path busca

--- config_rapidog/urls.py
+++ config_rapidog/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('produtos/alimentacao/', views.alimentacao),
     path('produtos/higiene/', views.higiene),
     path('produtos/brinquedos/', views.brinquedos),
-    path('petshops/', views.lojas)
+    path('petshops/', views.lojas),
+    path('busca/', views.busca),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
