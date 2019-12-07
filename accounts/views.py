66ad2eecba0dbd638ae9359fccef488885b1b836
leaandraz <leeschimit@gmail.com>
2019-12-07 13:02:05 -0300

    ajuste nas views de accounts

--- accounts/views.py
+++ accounts/views.py
@@ -3,9 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
-# Create your views here.
-
-class SignUp(generic.CreateView):
+class register(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
