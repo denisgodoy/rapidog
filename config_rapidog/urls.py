b8e4cb6e727a04ec8f96b504b7b24dff509782ef
leaandraz <leeschimit@gmail.com>
2019-12-10 00:22:40 -0300

    renderizar pagina contato.html

--- config_rapidog/urls.py
+++ config_rapidog/urls.py
@@ -33,4 +33,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', include('django.contrib.auth.urls')),
+    path('contato/', views.contato),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
