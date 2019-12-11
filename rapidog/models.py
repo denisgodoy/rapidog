e5b87dc6797a237162538426df14a8513ced3e0b
denisgodoy <denisgodoy1@gmail.com>
2019-12-11 17:54:23 -0300

    incluir campo codigo_loja na model Loja

--- rapidog/models.py
+++ rapidog/models.py
@@ -61,6 +61,7 @@ class Loja(models.Model):
     preco_combo = models.DecimalField(decimal_places=2, max_digits=20, default=0, blank=True, null=True)
     anunciar = models.BooleanField(default=False)
     anuncio = models.ImageField(upload_to='', null=True, blank=True)
+    codigo_loja = models.CharField(max_length=1000, blank=True)
 
     def __str__(self):
         return self.nome_loja
